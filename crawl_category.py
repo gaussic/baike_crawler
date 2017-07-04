645cf449c628938d3f2f9f88baffbd6fc016f677
gaussic <dzkangshu@hotmail.com>
2017-07-04 09:53:26 +0800

    crawl_category_flat

--- crawl_category.py
+++ crawl_category.py
@@ -17,7 +17,7 @@ def crawl_root_categories():
         big_cat = cat.find('h2').text
         small_categories = cat.find('dd').find_all('a')
         small_cat = [sc.text for sc in small_categories]
-        categories.append({big_cat: small_cat})
+        categories.append({"cat": big_cat, "sub_cat": small_cat})
     return categories
 
 
@@ -29,8 +29,8 @@ def parse_sub_categories(soup):
     return [a.text for a in category_list[0].find_all('a')]
 
 
-def crawl_sub_categories(category, depth):
-    """递归抓取下级分类"""
+def crawl_sub_categories_hierarchical(category, depth):
+    """递归抓取下级分类，生成层级分类"""
     if depth == 0:
         return []
     soup = crawl_html_doc('http://baike.baidu.com/fenlei/' + category)
@@ -40,7 +40,7 @@ def crawl_sub_categories(category, depth):
         if sub_cat in cat_set:  # 去重
             continue
         cat_set.add(sub_cat)
-        sub_c = crawl_sub_categories(sub_cat, depth - 1)
+        sub_c = crawl_sub_categories_hierarchical(sub_cat, depth - 1)
         if len(sub_c) != 0:
             subcat_list.append({'cat': sub_cat, 'sub_cat': sub_c})
         else:
@@ -50,10 +50,68 @@ def crawl_sub_categories(category, depth):
 
 
 def crawl_categories_hierarchical(depth=6):
-    sub_categories = crawl_sub_categories('政治人物', depth)
-    with open('t.json', 'w', encoding='utf-8') as f:
-        f.write(json.dumps(sub_categories, ensure_ascii=False, indent=4))
+    """生成层级分类列表"""
+    root_categories = crawl_root_categories()
+    categories = []
+    for rc in root_categories:
+        print('-------' + rc['cat'] + '----------')
+        sub_cat = []
+        for rc_sub in rc['sub_cat']:
+            sub_categories = crawl_sub_categories_hierarchical(rc_sub, depth)
+            sub_cat.append({'cat': rc_sub, 'sub_cat': sub_categories})
+        if len(sub_cat) != 0:
+            categories.append({'cat': rc['cat'], 'sub_cat': sub_cat})
+        else:
+            categories.append({'cat': rc['cat']})
+
+    with open('category_hierarchical.json', 'w', encoding='utf-8') as f:
+        f.write(json.dumps(categories, ensure_ascii=False, indent=4))
+
+    cat_set.clear()
+
+
+def crawl_sub_categories_flat(category, depth):
+    """递归抓取下级分类，生成平级分类"""
+    if depth == 0:
+        return []
+    soup = crawl_html_doc('http://baike.baidu.com/fenlei/' + category)
+    sub_categories = parse_sub_categories(soup)
+    subcat_list = []
+    for sub_cat in sub_categories:
+        if sub_cat in cat_set:  # 去重
+            continue
+        cat_set.add(sub_cat)
+        sub_c = crawl_sub_categories_flat(sub_cat, depth - 1)
+        if len(sub_c) != 0:
+            subcat_list.extend(sub_c)
+        else:
+            subcat_list.append(sub_cat)
+        print('Done: ' + sub_cat + ', depth=' + str(depth))
+    return subcat_list
+
+
+def crawl_categories_flat(depth=6):
+    """生层平级分类列表"""
+    cat_set.clear()
+    root_categories = crawl_root_categories()
+    categories = []
+    print(root_categories)
+    for rc in root_categories:
+        print('-------' + rc['cat'] + '----------')
+        sub_cat = []
+        sub_cat.extend(rc['sub_cat'])
+        for rc_sub in rc['sub_cat']:
+            sub_categories = crawl_sub_categories_flat(rc_sub, depth)
+            sub_cat.extend(sub_categories)
+        if len(sub_cat) != 0:
+            categories.append({'cat': rc['cat'], 'sub_cat': sub_cat})
+        else:
+            categories.append({'cat': rc['cat']})
+
+    with open('category_flat.json', 'w', encoding='utf-8') as f:
+        f.write(json.dumps(categories, ensure_ascii=False, indent=4))
 
 
 if __name__ == '__main__':
     crawl_categories_hierarchical()
+    crawl_categories_flat()
